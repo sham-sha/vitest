cf4511285408bbc21ffce3956430ece13f0985cd
sham-sha <sham08092003@gmail.com>
2025-06-09 22:46:48 +0530

    changes

---
+++ src/tests/fib.js
@@ -0,0 +1,5 @@
+function fib(){
+    console.log("jesus love you")
+}
+
+fib()
--- src/tests/sum.js
+++ src/tests/sum.js
@@ -2,8 +2,4 @@ export function sum(a, b) {
   return a + b;
 }
 
-function sam(b) {
-  return b ;
-  
-}
 console.log(sam(10));
